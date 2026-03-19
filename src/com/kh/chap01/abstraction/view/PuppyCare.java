43fcdec2aa85436cb25c04b4a7942844731840e1
인후이5110 <Yinhui5110@gmail.com>
2026-03-19 15:17:50 +0900

    객체지향 수업내용십구일

---
+++ src/com/kh/chap01/abstraction/view/PuppyCare.java
@@ -0,0 +1,19 @@
+package com.kh.chap01.abstraction.view;
+
+import java.util.Scanner;
+
+public class PuppyCare {
+
+	//사용자에게 강아지의 정보를 입력받아서
+	//입력받은 강아지의 정보를 출력해주고
+	//강아지와 함께 놀아줄 수 있는 프로그램
+	
+	public void run() {
+		System.out.println(" 🐕강아지 다마고치🐕");
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+	}
+	
+}
